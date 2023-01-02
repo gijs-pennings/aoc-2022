2c115e9204cba227d0b4e234beb5481df98a5161
Gijs Pennings <49961459+gijs-pennings@users.noreply.github.com>
2023-01-02 05:12:26 +0100

    Solve day 22 part 2

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -1,24 +1,23 @@
+private lateinit var nodes: List<List<Node>>
+private val w get() = nodes.size
+private val h get() = nodes[0].size
+
 fun main() {
     val lines = readInput(22)
     val map = lines.dropLast(2)
     val path = lines.last()
 
-    val w = map.maxOf { it.length }
-    val h = map.size
-    val nodes = List(w) { x ->
-        List(h) { y ->
-            Node(Pos(x, y), map[y].getOrNull(x) ?: ' ')
+    nodes = List(map.maxOf { it.length }) { x ->
+        List(map.size) { y ->
+            Node(FullPos.fromGlobal(x, y), map[y].getOrNull(x) ?: ' ')
         }
     }
     for (u in nodes.flatten())
-        for (f in Facing.VALS) {
-            var p = u.pos
-            do {
-                p = (p + f.d).floorMod(w, h)
-            } while (nodes[p].repr == ' ')
-            val v = nodes[p]
-            u.adj[f.ordinal] = if (v.repr == '.') v else u  // self-loop if neighbor is wall
-        }
+        if (u.repr != ' ')
+            for (f in Facing.VALS) {
+                val pair = next(u, f)
+                if (pair.first.repr == '.') u.adj[f.ordinal] = pair  // keep self-loop if neighbor is wall
+            }
 
     var i = 0
     var f = Facing.RIGHT
@@ -28,17 +27,68 @@ fun main() {
             val s = path.drop(i).takeWhile { it.isDigit() }
             i += s.length
             repeat(s.toInt()) {
-                u = u.adj[f.ordinal]
+                val pair = u.adj[f.ordinal]
+                u = pair.first
+                f = pair.second
             }
         } else {
             f = f.turn(path[i] == 'R')
             i++
         }
 
-    println(1000 * (u.pos.y + 1) + 4 * (u.pos.x + 1) + f.ordinal)
+    println(1000 * (u.pos.global.y + 1) + 4 * (u.pos.global.x + 1) + f.ordinal)
 }
 
-private fun Pos.floorMod(w: Int, h: Int) = Pos(Math.floorMod(x, w), Math.floorMod(y, h))
+private fun next(u: Node, f: Facing): Pair<Node, Facing> {
+    val global = u.pos.global + f.d
+    if (global.isWithin(w, h)) {
+        val v = nodes[global]
+        if (v.repr != ' ') return Pair(v, f)
+    }
+
+    val local = u.pos.local
+    return when (u.pos.side) {
+        0 -> {
+            if (f == Facing.LEFT)
+                Pair(nodes[FullPos.fromLocal(0, 49 - local.y, 3)], Facing.RIGHT)
+            else // up
+                Pair(nodes[FullPos.fromLocal(0, local.x, 5)], Facing.RIGHT)
+        }
+        1 -> when (f) {
+            Facing.RIGHT -> Pair(nodes[FullPos.fromLocal(49, 49 - local.y, 4)], Facing.LEFT)
+            Facing.DOWN -> Pair(nodes[FullPos.fromLocal(49, local.x, 2)], Facing.LEFT)
+            Facing.UP -> Pair(nodes[FullPos.fromLocal(local.x, 49, 5)], Facing.UP)
+            else -> throw IllegalStateException()
+        }
+        2 -> {
+            if (f == Facing.RIGHT)
+                Pair(nodes[FullPos.fromLocal(local.y, 49, 1)], Facing.UP)
+            else // left
+                Pair(nodes[FullPos.fromLocal(local.y, 0, 3)], Facing.DOWN)
+        }
+        3 -> {
+            if (f == Facing.LEFT)
+                Pair(nodes[FullPos.fromLocal(0, 49 - local.y, 0)], Facing.RIGHT)
+            else // up
+                Pair(nodes[FullPos.fromLocal(0, local.x, 2)], Facing.RIGHT)
+        }
+        4 -> {
+            if (f == Facing.RIGHT)
+                Pair(nodes[FullPos.fromLocal(49, 49 - local.y, 1)], Facing.LEFT)
+            else // down
+                Pair(nodes[FullPos.fromLocal(49, local.x, 5)], Facing.LEFT)
+        }
+        5 -> when (f) {
+            Facing.RIGHT -> Pair(nodes[FullPos.fromLocal(local.y, 49, 4)], Facing.UP)
+            Facing.DOWN -> Pair(nodes[FullPos.fromLocal(local.x, 0, 1)], Facing.DOWN)
+            Facing.LEFT -> Pair(nodes[FullPos.fromLocal(local.y, 0, 0)], Facing.DOWN)
+            else -> throw IllegalStateException()
+        }
+        else -> throw IllegalStateException("Illegal side: " + u.pos.side)
+    }
+}
+
+private operator fun <T> List<List<T>>.get(p: FullPos) = get(p.global)
 
 private enum class Facing(x: Int = 0, y: Int = 0) {
     RIGHT(x =  1),
@@ -55,9 +105,44 @@ private enum class Facing(x: Int = 0, y: Int = 0) {
     }
 }
 
+private class FullPos private constructor(
+    val global: Pos,
+    val local: Pos,
+    val side: Int,
+) {
+    companion object {
+        fun fromGlobal(p: Pos) = FullPos(p, p.floorMod(50, 50), getSide(p))
+        fun fromGlobal(x: Int, y: Int) = fromGlobal(Pos(x, y))
+        fun fromLocal(p: Pos, s: Int) = FullPos(getGlobal(p, s), p, s)
+        fun fromLocal(x: Int, y: Int, s: Int) = fromLocal(Pos(x, y), s)
+
+        private fun getSide(p: Pos): Int {
+            val sx = p.x / 50
+            val sy = p.y / 50
+            return when (sy) {
+                0 -> sx - 1
+                1 -> 2
+                2 -> sx + 3
+                3 -> 5
+                else -> throw IllegalStateException()
+            }
+        }
+
+        private fun getGlobal(local: Pos, side: Int) = local + 50 * when (side) {
+            0 -> Pos(1, 0)
+            1 -> Pos(2, 0)
+            2 -> Pos(1, 1)
+            3 -> Pos(0, 2)
+            4 -> Pos(1, 2)
+            5 -> Pos(0, 3)
+            else -> throw IllegalStateException()
+        }
+    }
+}
+
 private class Node(
-    val pos: Pos,
+    val pos: FullPos,
     val repr: Char
 ) {
-    val adj = Array(Facing.NUM) { this }
+    val adj = Facing.VALS.map { Pair(this, it) }.toTypedArray()
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -6,8 +6,13 @@ data class Pos(
     val x: Int,
     val y: Int
 ) {
+    fun floorMod(w: Int, h: Int) = Pos(Math.floorMod(x, w), Math.floorMod(y, h))
+    fun isWithin(w: Int, h: Int) = x in 0 until w && y in 0 until h
+
     operator fun plus(p: Pos) = Pos(x + p.x, y + p.y)
 }
+
+operator fun Int.times(p: Pos) = Pos(this * p.x, this * p.y)
 
 operator fun <T> List<List<T>>.get(p: Pos) = this[p.x][p.y]
 operator fun <T> List<MutableList<T>>.set(p: Pos, x: T) { this[p.x][p.y] = x }
